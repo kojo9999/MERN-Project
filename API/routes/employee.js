838aa6dd8f08b7b01a2ed3a731eca8b13d18128e
Sean Caldwell <sean.caldwell@datto.lan>
2023-02-21 10:47:30 +0000

    Create employee POST and GET requests

--- API/routes/employee.js
+++ API/routes/employee.js
@@ -1,4 +1,69 @@
 const express = require("express");
 const router = express.router();
-const item = require("../models/Employees");
+const Employee = require("../models/Employees");
 require("dotenv/config");
+const mongoose = require("mongoose");
+
+//CREATE NEW EMPLOYEE
+router.post("/createEmployee", async (req,res)=>{
+const{employeeId, firstName, lastName, dateOfBirth, email, active, age}=req.body;
+
+//create new employee object
+
+const employee = new Employee({
+    employeeId,
+    firstName, 
+    lastName, 
+    dateOfBirth, 
+    email, 
+    active, 
+    age
+});
+
+try{
+    //Save Employee to collection
+    await employee.save();
+
+    //return Employee success message
+    res.status(200).json({message:"Employee created Successfully"});
+
+}catch(err){
+    res.status(401).json({message:err.message});
+}
+});
+
+//GET ALL EMPLOYEES
+
+router.get("/getAllEmployees", async (req,res)=>{
+
+    try{
+        //get all employees
+        const allEmployees = await Employee.find({});
+
+        //return employee list
+        res.status(200).json({allEmployees});
+    }catch(err){
+        res.status(401).json({message:err.message})
+    }
+});
+
+//GET EMPLOYEES BY NAME
+router.get("/getEmployeesByName/:firstName", async (req,res)=>{
+
+    const firstName = req.params.firstName;
+
+    try{
+        //get  employees with matching name
+        const returnedEmployees = await Employee.find({
+            firstName: String(firstName),
+        });
+
+        //return employee list
+        res.status(200).json({returnedEmployees});
+    }catch(err){
+        res.status(401).json({message:err.message})
+    }
+
+});
+
+
