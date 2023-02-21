442e6eb6da7058d0b8a9143ae1eca899f04dc55b
Sean Caldwell <sean.caldwell@datto.lan>
2023-02-21 09:44:34 +0000

    Employee model
    Create employee model, skills to still be added

---
+++ models/Employees.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const EmployeeSchema = new mongoose.Schema({
+
+    employeeId:{
+        type: mongoose.Types.ObjectId,
+    },
+    firstName:{
+        type: String,
+    },
+    lastName:{
+        type:String,
+    },
+    dateOfBirth:{
+        type: Date,
+    },
+    email:{
+        type:String,
+        max:256
+    },
+    activite:{
+        type: Boolean,
+    },
+    age:{
+        type: Number
+    }
+});
+
+module.exports = mongoose.model("Employees",EmployeeSchema);
