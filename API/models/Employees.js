3492610545b857466c8999cd4009c51e64c3d3bb
Sean Caldwell <sean.caldwell@datto.lan>
2023-02-21 17:18:56 +0000

    Specifiy collection to use for Employees
    MongoDB pluralizes fields, adding specific naming convention

--- API/models/Employees.js
+++ API/models/Employees.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const EmployeeSchema = new mongoose.Schema({
 
     employeeId:{
-        type: mongoose.Types.ObjectId,
+        type: String,
     },
     firstName:{
         type: String,
@@ -24,6 +24,6 @@ const EmployeeSchema = new mongoose.Schema({
     age:{
         type: Number
     }
-});
+}, {collection:'Employees'});
 
 module.exports = mongoose.model("Employees",EmployeeSchema);
