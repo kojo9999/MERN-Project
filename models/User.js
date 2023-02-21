25bbfc21dc0d0e274283edd26c3c26bf36380086
Sean Caldwell <sean.caldwell@datto.lan>
2023-02-21 09:49:13 +0000

    User Model
    Model for user objects

---
+++ models/User.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+
+    id:{
+        type: mongoose.Types.ObjectId,
+    },
+    username:{
+        type: String,
+        min:6,
+        max:32,
+    },
+    password:{
+        type:String,
+        max:1024,
+        min:8,
+    }
+});
+
+module.exports = mongoose.model("Users",UserSchema);
