e4e48ca49d7088e3c1bfc6eec502782361d6f28b
Sean Caldwell <sean.caldwell@datto.lan>
2023-02-21 17:18:03 +0000

    Change datatype for ID to string
    Using ObjectId often caused bad calls to api, changed until later fix

--- API/models/User.js
+++ API/models/User.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema({
 
     id:{
-        type: mongoose.Types.ObjectId,
+        type: String,
     },
     username:{
         type: String,
