23c1341ebdd4fc73d62930e686ba84c789083855
Sean Caldwell <sean.caldwell@datto.lan>
2023-02-27 12:42:24 +0000

    Reducer to handle success/fail API register requests

---
+++ client/src/context/reducers.js
@@ -0,0 +1,27 @@
+import{REGISER_USER_BEGIN,REGISER_USER_SUCCESS,REGISER_USER_ERROR,LOGIN_USER_BEGIN,LOGIN_USER_ERROR,LOGIN_USER_SUCCESS} from './actions'
+
+const reducer =(state,action) =>{
+
+
+    if(action.type === REGISER_USER_BEGIN){
+        return {...state}
+    }
+
+    if(action.type===REGISER_USER_SUCCESS){
+        return{
+            ...state,
+            user:action.payload.user,
+            token:action.payload.accessToken
+        }
+    }
+
+    if(action.type===REGISER_USER_ERROR){
+        return{
+            ...state,
+        }
+    }
+
+    throw new Error(`no such action : ${action.type}`)
+}
+
+export default reducer
