37c5f6d881e8d61b2f82b271b1943279e91b9fa8
Sean Caldwell <sean.caldwell@datto.lan>
2023-02-28 17:40:54 +0000

    Update to CORS access control
    Updating CORS policy to allow parsing of accessToken from header

--- API/app.js
+++ API/app.js
@@ -12,8 +12,10 @@ app.use((req, res, next) => {
 //   res.setHeader("Pragma", "no-cache");
 //   res.setHeader("Expires", "0");
 res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
-  next();
+res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE,OPTIONS");
+res.set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+
+next();
  });
  
 
