dd9d6650f0a384db459aad776bfc0bfabcb50c35
Sean Caldwell <sean.caldwell@datto.lan>
2023-02-28 10:49:21 +0000

    Import and add Route for editEmployee Page

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import EmployeeList from './components/EmployeeList';
 import Dashboard from './views/Dashboard';
 import { BrowserRouter as Router,Routes,Route,Link} from 'react-router-dom';
 import Register from './views/Register';
+import EditEmployee from './views/EditEmployee'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path='/' element={<EmployeeList/>}></Route>
           <Route path='/register' element={<Register/>}></Route>
+          <Route path='/editEmployee/:employeeId' element={<EditEmployee/>}></Route>
 
         </Routes>
       </Router>
