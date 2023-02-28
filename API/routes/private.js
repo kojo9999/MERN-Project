fd994aa4f46e57a8490d9e7ea8c5d67a5a995ae6
Sean Caldwell <sean.caldwell@datto.lan>
2023-02-28 17:41:43 +0000

    UPdate private middleware
    Update to private middleware to parse accessToken from header instead of body

--- API/routes/private.js
+++ API/routes/private.js
@@ -1,20 +1,21 @@
 const jwt = require("jsonwebtoken");
 
-module.exports =function(req,res,next){
-    const token = req.body.accessToken
-
-    if(!token) return res.status(401).json("Access denied");
-
-    try{
-        //Verify token
-        const verified = jwt.verify(token, process.env.TOKEN_SECRET);
-
-        req.user = verified;
-
-        next();
-    }catch(err)
-    {
-        //return error message
-        res.status(401).json("invalid token");
+module.exports = function(req, res, next) {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+    console.log(token);
+  
+    if (!token) return res.status(401).json("Access denied");
+  
+    try {
+      // Verify token
+      const verified = jwt.verify(token, process.env.TOKEN_SECRET);
+  
+      req.user = verified;
+  
+      next();
+    } catch(err) {
+      // Return error message
+      res.status(401).json("invalid token");
     }
-}
+  }
