6967e38f5a8993557123aea3ab23fb3840bc682f
Sean Caldwell <sean.caldwell@datto.lan>
2023-03-01 17:17:32 +0000

    Importing and implementing "Private routes" into app.js

--- client/src/App.js
+++ client/src/App.js
@@ -8,21 +8,23 @@ import CreateEmployee from './views/createEmployee';
 import SkillList from './components/SkillList';
 import CreateSkillLevel from './views/CreateSkillLevel';
 import EditSkillLevel from './views/EditSkillLevel';
+import ProtectedRoutes from './components/ProtectedRoutes';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
+          <Route element={<ProtectedRoutes/>}>
           <Route path='/' element={<Dashboard/>}></Route>
           <Route path='/employeeList' element={<EmployeeList/>}></Route>
-          <Route path='/register' element={<Register/>}></Route>
           <Route path='/editEmployee/:employeeId' element={<EditEmployee/>}></Route>
           <Route path='/createEmployee' element={<CreateEmployee/>}></Route>
           <Route path='/skillList' element={<SkillList/>}></Route>
           <Route path='/createSkillLevel' element={<CreateSkillLevel/>}></Route>
           <Route path='/EditSkillLevel/:skillLevelId' element={<EditSkillLevel/>}></Route>
-
+          </Route>
+          <Route path='/register' element={<Register/>}></Route>
         </Routes>
       </Router>
     </div>
