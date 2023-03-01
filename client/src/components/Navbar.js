64aca2d2bb7bd9f896d2801350d0c9002adafada
Sean Caldwell <sean.caldwell@datto.lan>
2023-03-01 14:47:14 +0000

    Add refreshToken capabilities to navbar
    Method which checks users currect accessToken and refreshes if the users current token is valid

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,8 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
+import { useState, useEffect } from 'react';
+import axios from "axios";
+
 
 const Navbar =()=> {
+  
+  useEffect(() => {
+  
+    const token = JSON.parse(localStorage.getItem('refreshToken'));
+    const config ={
+      headers: { Authorization: `Bearer ${token}` }
+    };
+
+    axios.post('http://localhost:4000/api/auth/token', {},config)
+    .then(response => localStorage.setItem('accessToken',JSON.stringify(response.data.accessToken)))
+    .catch(error => {
+      console.error(error);
+      // handle error
+    });
+  });
+
+
   return (
     <nav className="navbar">
       <ul>
@@ -25,5 +45,7 @@ const Navbar =()=> {
     </nav>
   );
 }
+
+  
 
 export default Navbar;
