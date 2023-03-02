3ee9762ac58ae4c31d2399cb558e355a76e90054
Sean Caldwell <sean.caldwell@datto.lan>
2023-03-02 14:00:45 +0000

    Add basic input validation to Edit Skills
    Basic input validation to confirm input fields

--- client/src/views/CreateSkillLevel.js
+++ client/src/views/CreateSkillLevel.js
@@ -2,12 +2,13 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import '../App.css';
 import Navbar from '../components/Navbar';
-import { useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom';
 
 function CreateSkill() {
   const [skillName, setSkillName] = useState('');
   const [skillDesc, setSkillDesc] = useState('');
-  const navigate = useNavigate()
+  const [errors, setErrors] = useState([]);
+  const navigate = useNavigate();
   const token = JSON.parse(localStorage.getItem('accessToken'));
   const config ={
     headers: { Authorization: `Bearer ${token}` }
@@ -16,38 +17,73 @@ function CreateSkill() {
   const handleSubmit = (event) => {
     event.preventDefault();
 
+    // Validate form data
+    const validationErrors = [];
+    if (skillName.trim() === '') {
+      validationErrors.push('Please enter a skill name');
+    }
+    if (skillDesc.trim() === '') {
+      validationErrors.push('Please enter a skill description');
+    }
+    setErrors(validationErrors);
+
+    // If there are any validation errors, don't submit the form
+    if (validationErrors.length > 0) {
+      return;
+    }
+
     // Submit form data to the API
     const formData = {
       skillName,
       skillDesc,
     };
 
-    console.log(formData);
-
-    axios.post('/api/skillLevel/createNewSkillLevel', formData,config)
-    .then((response) => {
-      console.log(response.data);
-    });
-    navigate('/skillList')
+    axios.post('/api/skillLevel/createNewSkillLevel', formData, config)
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+    navigate('/skillList');
   };
 
   return (
     <div>
-      <Navbar/>
-    <div className="edit-employee-container">
-      <form className="edit-employee-form" onSubmit={handleSubmit}>
-        <h2>Create New Skill</h2>
-        <label>
-          Skill Name:
-          <input type="text" name="skillName" value={skillName} onChange={(e) => setSkillName(e.target.value)} />
-        </label>
-        <label>
-          Skill Description:
-          <input type="text" name="skillDesc" value={skillDesc} onChange={(e) => setSkillDesc(e.target.value)} />
-        </label>
-        <button className="submit-button" type="submit">Create</button>
-      </form>
-    </div>
+      <Navbar />
+      <div className="edit-employee-container">
+        <form className="edit-employee-form" onSubmit={handleSubmit}>
+          <h2>Create New Skill</h2>
+          {errors.length > 0 && (
+            <div className="error">
+              {errors.map((error, index) => (
+                <div key={index}>{error}</div>
+              ))}
+            </div>
+          )}
+          <label>
+            Skill Name:
+            <input
+              type="text"
+              name="skillName"
+              value={skillName}
+              onChange={(e) => setSkillName(e.target.value)}
+            />
+          </label>
+          <label>
+            Skill Description:
+            <input
+              type="text"
+              name="skillDesc"
+              value={skillDesc}
+              onChange={(e) => setSkillDesc(e.target.value)}
+            />
+          </label>
+          <button className="submit-button" type="submit">
+            Create
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
