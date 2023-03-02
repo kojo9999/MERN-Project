07c8dc28e47fca801c6d20f2f1e20bfe79374e0e
Sean Caldwell <sean.caldwell@datto.lan>
2023-03-02 14:02:36 +0000

    Add basic input validation to Register page

--- client/src/views/Register.js
+++ client/src/views/Register.js
@@ -1,108 +1,119 @@
-import { formToJSON } from 'axios'
-import React, {useState,useEffect}from 'react'
+import React, { useState, useEffect } from 'react'
 import FormInput from '../components/FormInput'
 import { useAppContext } from '../context/appContext'
 import { useNavigate } from 'react-router-dom'
 
-const initalState={
-    username:'',
-    email:'',
-    password:'',
-    isMember: false,
+const initialState = {
+  username: '',
+  email: '',
+  password: '',
+  isMember: false,
 }
 
-
 const Register = () => {
+  const [values, setValues] = useState(initialState)
+  const [errors, setErrors] = useState({})
+  const { registerUser, user, loginUser } = useAppContext()
+  const navigate = useNavigate()
 
-    const [values,setValues] =useState(initalState)
+  const toggleMember = () => {
+    setValues({ ...values, isMember: !values.isMember })
+  }
 
-    const {registerUser,user,loginUser} = useAppContext()
-    const navigate = useNavigate()
+  const handleChange = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value })
+  }
 
-    const toggleMember = () => {
-        setValues({...values,isMember:!values.isMember})
+  const validate = (fieldValues = values) => {
+    let tempErrors = {}
+
+    if ('username' in fieldValues) {
+      tempErrors.username = fieldValues.username ? '' : 'Username is required'
     }
 
-    const handleChange = e =>{
-        setValues({...values,[e.target.name]:e.target.value})
+    if ('email' in fieldValues) {
+      tempErrors.email = fieldValues.email
+        ? /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(fieldValues.email)
+          ? ''
+          : 'Invalid email address'
+        : 'Email is required'
     }
 
-    const handleSubmit = e =>{
-        e.preventDefault()
-        
-
-        const {username,email,password,isMember} = values;
-
-        if(!username||!password||(!isMember&&!username)){
-            return
-        }
-
-        const currentUser = {username,email,password}
-        if(isMember){
-            loginUser(currentUser)
-        }else{
-            registerUser(currentUser)
-        }
-
+    if ('password' in fieldValues) {
+      tempErrors.password = fieldValues.password ? '' : 'Password is required'
     }
 
-    useEffect(()=>{
+    setErrors(tempErrors)
+  }
 
-        if(user){
-            setTimeout(()=>{
-                
-                navigate('/')
+  const handleSubmit = (e) => {
+    e.preventDefault()
 
-            },3000)
-        }
+    validate()
 
-    },[user,navigate])
+    if (Object.keys(errors).length !== 0) {
+      return
+    }
+
+    const { username, email, password, isMember } = values
+
+    const currentUser = { username, email, password }
+
+    if (isMember) {
+      loginUser(currentUser)
+    } else {
+      registerUser(currentUser)
+    }
+  }
+
+  useEffect(() => {
+    if (user) {
+      setTimeout(() => {
+        navigate('/')
+      }, 3000)
+    }
+  }, [user, navigate])
 
   return (
     <form onSubmit={handleSubmit}>
+      <h1>{values.isMember ? 'Login' : 'Register'}</h1>
+      {!values.isMember && (
+        <FormInput
+          type='email'
+          value={values.email}
+          name='email'
+          labelText='Email'
+          handleChange={handleChange}
+          errorMessage={errors.email}
+        />
+      )}
 
-    <h1>{values.isMember?'Login':'Register'}</h1>
-    
-    {!values.isMember && (
+      <FormInput
+        type='text'
+        value={values.username}
+        name='username'
+        labelText='Username'
+        handleChange={handleChange}
+        errorMessage={errors.username}
+      />
 
-<FormInput
+      <FormInput
+        type='password'
+        value={values.password}
+        name='password'
+        labelText='Password'
+        handleChange={handleChange}
+        errorMessage={errors.password}
+      />
 
+      <button type='submit'>Submit</button>
 
-type='email'
-value={values.email}
-name='email'
-handleChange={handleChange}
-
-/>
-
-    )}
-
-   
-
-<FormInput
-
-type='text'
-value={values.username}
-name='username'
-handleChange={handleChange}
-
-/>
-
-<FormInput
-
-type='password'
-value={values.password}
-name='password'
-handleChange={handleChange}
-
-/>
-
-    <button type='submit'>Submit</button>
-
-    <p>{!values.isMember ? 'Already a member?':'Not a member yet?'}</p>
-    <button type='button' onClick={toggleMember}>{!values.isMember ? 'Login':'Register'}</button>
-
+      <p>{!values.isMember ? 'Already a member?' : 'Not a member yet?'}</p>
+      <button type='button' onClick={toggleMember}>
+        {!values.isMember ? 'Login' : 'Register'}
+      </button>
     </form>
   )
 }
+
 export default Register
