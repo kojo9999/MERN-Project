ade2817962f18d121c096e841aa84c0996fa958a
kojo9999 <Seancaldwell00@gmail.com>
2023-03-09 13:58:23 +0000

    Added additional tests for cache and code coverage
    Added repeat tests to test cache and to increase code/brach coverage

--- API/models/Employees.unit.test.js
+++ API/models/Employees.unit.test.js
@@ -8,6 +8,7 @@ const mongoose = require("mongoose")
 const user = { _id: '1234567890' };
 const token = "Bearer "+jwt.sign(user, process.env.TOKEN_SECRET, { expiresIn: '60m' });
 let updated="";
+let employeFirstName ="John"
 
 
 
@@ -50,6 +51,20 @@ describe("GET /employees/getAllEmployees", () => {
     });
   });
 
+  describe("GET /employees/getAllEmployees from cache", () => {
+    it("should return a list of all employees", async () => {
+  
+      const response = await request(app)
+        .get("/api/employee/getAllEmployees")
+        .set("Authorization", token);
+  
+      expect(response.statusCode).toBe(200);
+      expect(response.body.Employees).toBeDefined();
+      expect(Array.isArray(response.body.Employees)).toBe(true);
+    });
+  });
+
+
   describe("GET /getEmployeeById/:employeeId", () => {
     it("responds with status 200 and a single employee with matching ID", async () => {
   
@@ -68,6 +83,56 @@ describe("GET /employees/getAllEmployees", () => {
       // Expect the employee in the response to have an ID that matches the search term
       expect(response.body.employees[0].employeeId).toBe(employeeId);
     });
+  });
+
+  describe("GET /getEmployeeById/:employeeId from cache", () => {
+    it("responds with status 200 and a single employee with matching ID", async () => {
+  
+      // Define an employee ID to search for
+      const employeeId = "1234567890";
+  
+      // Send a GET request to the route with the JWT token and employee ID in the URL
+      const response = await request(app)
+        .get(`/api/employee/getEmployeeById/${employeeId}`)
+        .set("Authorization", token);
+  
+      // Expect the response status to be 200 and the response body to be an object
+      expect(response.status).toBe(200);
+      expect(typeof response.body.employees).toBe("object");
+  
+      // Expect the employee in the response to have an ID that matches the search term
+      expect(response.body.employees[0].employeeId).toBe(employeeId);
+    });
+  });
+
+  it("should return a list of employees with matching first name", async () => {
+    const res = await request(app).get("/api/employee/getEmployeesByName/John")
+    .set("Authorization", token);
+
+    expect(res.statusCode).toEqual(200);
+    expect(res.body.returnedEmployees).toHaveLength(1);
+    expect(res.body.returnedEmployees[0].firstName).toEqual("John");
+    expect(res.body.returnedEmployees[0].lastName).toEqual("Doe");
+    expect(res.body.returnedEmployees[0].dateOfBirth).toEqual("1980-01-01");
+    expect(res.body.returnedEmployees[0].email).toEqual("john.doe@example.com");
+    expect(res.body.returnedEmployees[0].skills).toEqual([]);
+    expect(res.body.returnedEmployees[0].active).toEqual(true);
+    expect(res.body.returnedEmployees[0].age).toEqual(41);
+  });
+
+  it("should return a list of employees with matching first name from cache", async () => {
+    const res = await request(app).get("/api/employee/getEmployeesByName/John")
+    .set("Authorization", token);
+
+    expect(res.statusCode).toEqual(200);
+    expect(res.body.Employees).toHaveLength(1);
+    expect(res.body.Employees[0].firstName).toEqual("John");
+    expect(res.body.Employees[0].lastName).toEqual("Doe");
+    expect(res.body.Employees[0].dateOfBirth).toEqual("1980-01-01");
+    expect(res.body.Employees[0].email).toEqual("john.doe@example.com");
+    expect(res.body.Employees[0].skills).toEqual([]);
+    expect(res.body.Employees[0].active).toEqual(true);
+    expect(res.body.Employees[0].age).toEqual(41);
   });
 
   describe("PUT /employees/updateEmployee/:employeeId", () => {
@@ -110,6 +175,14 @@ describe("GET /employees/getAllEmployees", () => {
       expect(updatedResponse.body.employees[0].active).toBe(updatedEmployee.active);
       updated=updatedResponse.body.employees[0]._id;
     });
+  });
+
+
+
+  it("should return an empty list if no employees match first name", async () => {
+    const res = await request(app).get("/api/employee/getEmployeesByName/George");
+
+    expect(res.statusCode).toEqual(401);
   });
   
   describe("DELETE /employees/deleteEmployee/:employeeId", () => {
